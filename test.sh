1772bb4a94f0abf79dd8e7f68aa1dcb33ac19611
Unknown <andrepinheiro.projectos@gmail.com>
2018-07-17 17:24:50 +0100

    #AP# testing TRAVIS CI

--- test.sh
+++ test.sh
@@ -2,8 +2,11 @@
 set -e
 
 actual=`docker exec -it mysql mysql -uroot -ppassword -e 'show databases;'`
+actual2=`docker ps -a`
 echo "Server says: $actual"
-if [ "$actual" == *_${dbname}_* ]; then
+echo "Server says: $actual2"
+
+if [ "$actual2" == *_${Up}_* ]; then
   echo "Test failed"
   exit 1
 else
