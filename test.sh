86ab11b7f09a4eaf7024e512677327a9e20708c5
Unknown <andrepinheiro.projectos@gmail.com>
2018-07-17 17:27:38 +0100

    #AP# Another fix on test script

--- test.sh
+++ test.sh
@@ -2,14 +2,4 @@
 set -e
 
 actual=`docker exec -it mysql mysql -uroot -ppassword -e 'show databases;'`
-actual2=`docker ps -a`
 echo "Server says: $actual"
-echo "Server says: $actual2"
-
-if [ "$actual2" == *_${Up}_* ]; then
-  echo "Test failed"
-  exit 1
-else
-  echo "Test passed"
-  exit 0
-fi
